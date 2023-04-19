a9788bc9ea8e2ef16f04ced9b6e9daa351cf2cfa
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-19 04:06:00 +0200

    [skip ci] update docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,14 +5,12 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'gerstnerhungarian'
+project = 'GothicHungarian'
 copyright = '2023, Viktor Martinović'
 author = 'Viktor Martinović'
-version = '2.0'
-release = '2.0'
+version = '1.0'
+release = '1.0'
 extensions = ['sphinx.ext.autodoc']
 html_theme = 'sphinx_rtd_theme'
 # TODO replace mock with docs/requirements.txt after loanpy-release
-autodoc_mock_imports = ["epitran", "loanpy", "lingpy", "clldutils", "tabulate",
-                        "attr", "pylexibank", "cldfbench", "spacy",
-                        "lexibank_gerstnerhungarian", "pycldf", "pysem"]
+autodoc_mock_imports = ["spacy"]
