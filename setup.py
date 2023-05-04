b9e82bd7bbbd0f79cfcb01f11ee4d8de5d99f89d
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-05-04 22:52:00 +0200

    updatesetuppyandciworkflow

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="GothicHungarian",
     version="1.0",
     packages=find_packages(),
-    install_requires = "spacy>=3.5.2",
+    install_requires = ["spacy>=3.5.2", "loanpy==3.0.0"]
     entry_points={
         "console_scripts": [
             "loadinput = gothuncommands.loadinput:main",
